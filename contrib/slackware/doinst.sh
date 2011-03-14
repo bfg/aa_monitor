450ad832392c7edd67c305a89bf8fd4b920710e3
Brane F. Gračnar <bfg@frost.ath.cx>
2011-03-14 17:48:35 +0100

    Updated package build scripts.

--- contrib/slackware/doinst.sh
+++ contrib/slackware/doinst.sh
@@ -1,13 +1,5 @@
 #!/bin/sh
 
-# $Id: doinst.sh 771 2008-04-10 12:05:41Z bfg $
-# $Date: 2008-04-10 14:05:41 +0200 (Thu, 10 Apr 2008) $
-# $Author: bfg $
-# $Revision: 771 $
-# $LastChangedRevision: 771 $
-# $LastChangedBy: bfg $
-# $LastChangedDate: 2008-04-10 14:05:41 +0200 (Thu, 10 Apr 2008) $
-
 config() {
 	NEW="$1"
 	OLD="`dirname $NEW`/`basename $NEW .new`"

52dbba520e77e3987c60393a0d5b719b65c967b0
Brane F. Gračnar <bfg@frost.ath.cx>
2011-04-07 11:53:49 +0200

    Added changelog.

---
+++ lib/P9/AA/CHANGELOG.pm
@@ -0,0 +1,46 @@
+=head1 NAME
+
+aa_monitor changelog
+
+=head1 VERSIONS
+
+=head2 1.0.3
+
+=over
+
+=item * added CHANGELOG pod
+
+=item * module: finished L<B<MongoDB>|P9::AA::Check::MongoDB>.
+
+=item * module: finished L<B<MongoDBReplicaSet>|P9::AA::Check::MongoDBReplicaSet>.
+
+=item * module L<B<DBIReplication>|P9::AA::Check::DBIReplication>:
+Fixed minor typo that caused check to fail.
+
+=item * config: added configuration parameter B<max_execution_time>
+
+=item * basic daemon implementation
+
+=over
+
+=item * check for zombie kids (respect max_execution_time) before each accept()
+
+=item * report what's going on in SIGCHLD handler in debug mode
+
+=item * reconfigure logger on SIGHUP
+
+=back
+
+=back
+
+=head2 1.0.2
+
+=over
+
+=item * bundled module L<Net::INET6Glue>
+
+=item * Debian package fixes.
+
+=back
+
+=cut

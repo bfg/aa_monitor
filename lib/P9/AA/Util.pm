18ea1efd8499a308610941765e16b787f64a8bd0
Brane F. Gračnar <bfg@frost.ath.cx>
2011-06-17 16:19:48 +0200

    Added JSON and XML parsing methods.

--- lib/P9/AA/Util.pm
+++ lib/P9/AA/Util.pm
@@ -3,13 +3,18 @@ package P9::AA::Util;
 use strict;
 use warnings;
 
+use IO::File;
 use Data::Dumper;
 use Text::ParseWords;
 use Scalar::Util qw(blessed);
 
+use constant MAX_FILE_SIZE => 1024 * 1024 * 4;
+
 our $VERSION = 0.12;
 
 my $_obj = undef;
+my $_has_json = undef;
+my $_has_xml = undef;
 
 =head1 NAME
 
@@ -247,7 +252,7 @@ sub dumpVarCompact {
 	return $d->Dump();
 }
 
-=head2 newId ()
+=head2 newId
 
 Returns new random id as 8 character string.
 
@@ -296,6 +301,241 @@ sub getBaseUrl {
 	#print STDERR "base url from uri '$uri' => '$u'\n";
 	
 	return $u;
+}
+
+=head2 getJsonParser
+
+Returns UTF-8 enabled and relaxed L<JSON> parser object on success, otherwise undef.
+
+=cut
+sub getJsonParser {
+	my $self = shift;
+	return undef unless ($self->hasJSON());
+		
+	my $jp = JSON->new();
+	$jp->utf8(1);
+	$jp->relaxed(1);
+	return $jp;
+}
+
+=head2 parseJsonFile
+
+ my $s = $u->parseJsonFile($filename);
+ my $j = $u->parseJsonFile($filename, 1024 * 1024);	# parse $filename, but only if it's smaller than 1MiB
+
+Tries to parse specified JSON file and returns hash or array reference on success, otherwise undef.
+
+=cut
+sub parseJsonFile {
+	my ($self, $file, $maxlen) = @_;
+	$maxlen = MAX_FILE_SIZE unless (defined $maxlen);
+	
+	# try to stat file
+	my @s = stat($file);
+	unless (@s) {
+		$self->error("Unable to stat JSON file '$file': $!");
+		return undef;
+	}
+	
+	# too big file?
+	if ($maxlen > 0 && $s[7] > $maxlen) {
+		$self->error(
+			"JSON file size too big: $s[7] bytes. " .
+			"I'm unwilling to parse (max allowrd size: $maxlen bytes)."
+		);
+		return undef;
+	}
+	
+	# read file
+	my $buf = $self->readFile($file, $maxlen);
+	return undef unless (defined $buf);
+	
+	# now try to decode json
+	return $self->parseJson(\ $buf);
+}
+
+=head2 parseJson
+
+ my $s = $u->parseJson($str);
+ my $s = $u->parseJson(\ $str);
+
+Tries to parse specified string or scalar reference containing JSON string. Returns
+hash or array reference on success, otherwise undef.
+
+=cut
+sub parseJson {
+	my ($self, $str) = @_;
+	unless (defined $str) {
+		$self->error("Undefined JSON data.");
+		return undef;
+	}
+	my $buf = (ref($str) eq 'SCALAR') ? $str : \ $str;
+
+	# get json parser
+	my $jp = $self->getJsonParser();
+	return undef unless (defined $jp);
+
+	# try to decode json
+	local $@;
+	my $s = eval { $jp->decode($$buf) };
+	if ($@) {
+		my $e = $@;
+		$e =~ s/[\r\n]+$//g;
+		$self->error("Error decoding JSON: " . $e);
+		return undef;
+	}
+	
+	return $s;
+}
+
+=head2 getXmlParser
+
+ my $parser = $u->getXmlParser();
+
+Returns initialized L<XML::Simple> object on success, otherwise undef.
+
+=cut
+sub getXmlParser {
+	my ($self) = @_;
+	return undef unless ($self->hasXML());
+	return XML::Simple->new();
+}
+
+=head2 parseXml
+
+ my $s = $u->parseXml($str);
+ my $s = $u->parseXml(\ $str);
+
+Tries to parse specified string or scalar reference containing XML string. Returns
+hash reference on success, otherwise undef.
+
+=cut
+sub parseXml {
+	my ($self, $str) = @_;
+	unless (defined $str) {
+		$self->error("Undefined XML data.");
+		return undef;
+	}
+	my $buf = (ref($str) eq 'SCALAR') ? $str : \ $str;
+	
+	# create parser object...
+	my $p = $self->getXmlParser();
+	return undef unless (defined $p);
+
+	# try to parse
+	local $@;
+	my $d = eval { $p->parse_string($buf) };
+	if ($@) {
+		$self->error("Error parsing XML: syntax error.");
+		return undef;
+	}
+	elsif (! defined $d || ref($d) ne 'HASH') {
+		$self->error("Error parsing XML: parser returned invalid structure.");
+		return undef;
+	}
+	
+	return $d;
+}
+
+=head2 readFile
+
+ my $buf = $u->readFile($filename);         # return whole file
+ my $str = $u->readFile($filename, 1023);   # return only first 1024 bytes of file
+
+Tries to read file specified in argument and returns it's contents on success, otherwise undef.
+
+
+B<NOTE:> Argument $filename can be '-' for STDIN, file name, raw filedescriptor or glob reference.
+
+=cut
+sub readFile {
+	my ($self, $file, $maxlen) = @_;
+	{
+		no warnings;
+		$maxlen = 0 unless (defined $maxlen || $maxlen < 0);
+	}
+
+	unless (defined $file && length($file) > 0) {
+		$self->error("Unable to read file: undefined filename.");
+		return undef;
+	}
+
+	# Open file
+	my $fd = undef;
+	if ($file eq '-') {
+		$fd = IO::Handle->new();
+		unless ($fd->fdopen(fileno(STDIN), 'r')) {
+			$self->error("Unable to open stdin for reading: $!");
+			$fd = undef;
+		}
+	}
+	elsif (blessed($file) && $file->isa('IO::File')) {
+		$fd = $file;
+	}
+	elsif (fileno($file)) {
+		$fd = IO::Handle->new();
+		unless ($fd->fdopen(fileno($file), 'r')) {
+			$self->error("Unable to open special handle for reading: $!");
+			$fd = undef;
+		}
+	}
+	else {
+		$fd = IO::File->new($file, 'r');
+		unless (defined $fd) {
+			$self->error("Unable to open file $file for reading: $!");
+			$fd = undef;
+		}
+	}
+	return undef unless (defined $fd);
+	
+	# read file
+	use bytes;
+	my $buf = '';
+	my $nread = 0;
+	while (<$fd>) {
+		my $len = length($_);
+		if ($maxlen > 0) {
+			# read too many?
+			if (($nread + $len) > $maxlen) {
+				# add leftovers if any
+				my $diff = abs($maxlen - ($nread + $len));
+				if ($diff > 0) {
+					$buf .= substr($_, 0, $diff);
+				}
+				# stop reading
+				last;
+			} else {
+				$nread += $len;
+				$buf .= $_;
+			}
+		} else {
+			$buf .= $_;
+		}
+	}
+	
+	return $buf;
+}
+
+sub hasJSON {
+	my $self = shift;
+	unless (defined $_has_json) {
+		local $@;
+		$_has_json = eval { require JSON };	
+	}
+	
+	$self->error("Module JSON is not available.") unless ($_has_json);
+	return $_has_json;
+}
+
+sub hasXML {
+	my $self = shift;
+	unless (defined $_has_xml) {
+		local $@;
+		$_has_xml = eval { require XML::Simple };
+	}
+
+	$self->error("Module XML::Simple is not available.") unless ($_has_xml);
+	return $_has_xml;
 }
 
 sub _error {

f557f9e59d887667c1e670cf4fd568c843bb0d65
Brane F. Gračnar <bfg@frost.ath.cx>
2012-02-02 17:52:36 +0100

    IOIPTV refactoring
    * refactored checking of IOIPTV module and added min_byteps argument
    * bumped version number and updated changelog

--- lib/P9/AA/Check/IOIPTV.pm
+++ lib/P9/AA/Check/IOIPTV.pm
@@ -7,7 +7,7 @@ use P9::AA::Constants;
 use base 'P9::AA::Check::XML';
 
 # version MUST be set
-our $VERSION = 0.10;
+our $VERSION = 0.11;
 
 =head1 NAME
 
@@ -25,10 +25,12 @@ sub clearParams {
 		"IO IPTV check."
 	);
 
-	# you can also remove any previously created
-	# configuration parameter.
-	# $self->cfgParamRemove('debug');
-	
+	$self->cfgParamAdd(
+		'min_byteps',
+		0,
+		'Minimum byteps rate.',
+		$self->validate_int(0)
+	);	
 	# this method MUST return 1!
 	return 1;
 }
@@ -38,7 +40,8 @@ sub check {
 	my ($self) = @_;
 	
 	# get XML
-	my ($xml, $xml_str) = $self->getXML();
+	my $p = $self->getXMLParser(ForceArray => 0);
+	my ($xml, $xml_str) = $self->getXML(parser => $p);
 	return CHECK_ERR unless (defined $xml && ref($xml) eq 'HASH');
 	
 	if ($self->{debug}) {
@@ -47,57 +50,76 @@ sub check {
 		$self->bufApp("--- END XML STRUCT ---");
 	}
 	
+	my $res = CHECK_OK;
+	
 	my $task = (exists($xml->{task}) && ref($xml->{task}) eq 'ARRAY') ? $xml->{task} : undef;
-	unless (defined $task) {
-		return $self->error("XML doesn't contain task element.");
+	if (! defined $task) {
+		# try to validate single element
+		return $self->_checkChannel($xml);
+	} else {
+		my $i = 0;
+		foreach my $e (@{$task}) {
+			$i++;
+			my $r = $self->_checkChannel($e, $i);
+			if ($r != CHECK_OK) {
+				$res = $r unless ($res == CHECK_ERR);
+			}
+		}
 	}
+	
+	return $res;
+}
 
-	# validate contents
+sub _checkChannel {
+	my ($self, $e, $i) = @_;
+	$i = 1 unless (defined $i);
+	unless (defined $e && ref($e) eq 'HASH') {
+		no warnings;
+		return $self->error($self->error() . "\nNot a hash reference.");
+	}
+	
 	my $res = CHECK_OK;
 	my $err = '';
 	my $warn = '';
 	
-	my $i = 0;
-	foreach my $e (@{$task}) {
-		$i++;
-		unless (defined $e && ref($e) eq 'HASH') {
-			$warn .= 'Invalid XML task element $i: Not a valid reference: ' .
-				$self->dumpVarCompact($e) . "\n";
-			$res = CHECK_WARN unless ($res == CHECK_ERR);
-			next;
-		}
-		my $id = $e->{_id} || undef;
-		my $bps = $e->{byteps} || undef;
-		my $active = $e->{active} || undef;
-		unless (defined $id && defined $bps && defined $active) {
-			$warn .= 'Invalid XML task element $i: id, bps and active are not defined: ' .
-				$self->dumpVarCompact($e) . "\n";
-			$res = CHECK_WARN unless ($res == CHECK_ERR);
-			next;
-		}
-		
-		$active = lc($active);
-		my ($name, $type) = split(/\s*[;,]\s*/, $id, 2);
-		unless (defined $name && defined $type) {
-			$warn .= 'Invalid XML task element $i: id doesn\'t contain name and type ' .
-				$self->dumpVarCompact($e) . "\n";
-			$res = CHECK_WARN unless ($res == CHECK_ERR);
-			next;
-		}
+	# $self->bufApp("HAHAHAHA: " . $self->dumpVar($e));
 
-		# ok, now do the real check...
-		unless ($active eq 'true' || $active eq 'yes' || $active eq '1') {
-			$err .= "Channel $name is not active.\n";
-			$res = CHECK_ERR;
-		}
+	my $id = $e->{_id} || undef;
+	my $bps = eval { no warnings; int($e->{byteps}) } || 0;
+	my $active = $e->{active} || undef;
+	unless (defined $id && defined $bps && defined $active) {
+		$warn .= "Invalid XML task element: id, bps and active are not defined.\n";
+		$res = CHECK_WARN unless ($res == CHECK_ERR);
+	}
+
+	$active = lc($active);
+	my ($name, $type) = split(/\s*[;,]\s*/, $id, 2);
+#	unless (defined $name && defined $type) {
+#		$warn .= "Invalid XML task element $i: id doesn't contain name and type.\n";
+#		$res = CHECK_WARN unless ($res == CHECK_WARN);
+#	}
+
+	# ok, now do the real check...
+	unless ($active eq 'true' || $active eq 'yes' || $active eq '1') {
+		$err .= "Channel $name is not active.\n";
+		$res = CHECK_ERR;
 	}
 	
-
+	# check rate
+	if ($self->{min_byteps} > 0 && $bps < $self->{min_byteps}) {
+		$err .= "Rate $bps B/s is lower than specified rate of $self->{min_byteps} B/s.\n";
+		$res = CHECK_ERR;
+	}
+	
 	unless ($res == CHECK_OK) {
-		$err =~ s/\s+$//g;
-		$warn =~ s/\s+$//g;
-		$self->warning($warn) if (length($warn));
-		$self->error($err) if (length($err));
+		no warnings;
+		my $pfx = "Channel [" . ((defined $name) ? $name : $i) . "]: ";
+		if ($res == CHECK_WARN) {
+			$self->warning($self->warning() . $pfx . $warn);
+		}
+		if ($res == CHECK_ERR) {
+			$self->error($self->error() . $pfx . $err);
+		}
 	}
 
 	return $res;
